727aa4fbf2f3bf3f5183435394e4114098d40207
SharifHossen <Sharif.Hossen@brainstation-23.com>
2025-07-17 10:21:13 +0600

    composition API

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref<number>(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
